f4c08d72353d16872e3952dba7b8f082f601730f
Your Namejaymahallle <your-emailmahallejay4@gamil.com>
2026-08-14 15:26:05 +0530

    change save

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext' // Path verify kar lein
+import { AuthProvider } from './context/AuthContext.jsx' // Path verify kar lein
 import { JobProvider } from './context/JobContext.jsx'
 
 createRoot(document.getElementById('root')).render(
